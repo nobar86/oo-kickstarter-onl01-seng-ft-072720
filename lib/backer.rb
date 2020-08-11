84cc614ccf48160b9b03d65cdfd0a0f48f3f5e97
Michael Rabon <nobar86@gmail.com>
2020-08-11 21:54:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,14 @@
 class Backer
   attr_reader :name
   
- 
+ @backed_projects = []
   
   def initialize(name)
     @name = name
-     @backed_projects = []
   end
   
+  def backed_projects
+    
+  end
   
 end
